faad13c4384aa17743202865b8c0db98fe797b1a
Antkilla <132300802+Antkilla@users.noreply.github.com>
2023-11-02 19:11:26 -0700

    Update MovieDetailsPage.jsx

--- src/pages/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchMovieById } from '../services/api'
 
 const MovieDetailsPage = () => {
   const { id } = useParams();
+  const [MovieDetails, setMovieDetails] = useState(null);
 
+  useEffect(() => {
+    const fetchMovieDetails = async () => {
+      try {
+        const details = await fetchMovieById(id);
+        setMovieDetails(details);
+      } catch (error) {
+        console.error('Error fetching movie details:', error);
+      }
+    }
+
+    fetchMovieDetails();
+  }, [id])
+
+  if (!MovieDetails) {
+    return <p>Loading...</p>;
+  }
+  
   return (
     <div>
       <h2>Movie Details</h2>
+      <h3>Title: {MovieDetails.Title}</h3>
+      <p>Year: {MovieDetails.Year}</p>
+      <p>Genre: {MovieDetails.Genre}</p>
       <MovieDetailsPage movieId={id} />
     </div>
   );
